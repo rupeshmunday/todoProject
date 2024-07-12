9657888a6f00ddd2f20d0e4ef32f24c28602a7da
rupeshmunday <pra6305@gmail.com>
2024-07-12 20:28:18 +0530

    Frontend chaged homepage

---
+++ Frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+import './CreateTodo.css'
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title" /> <br />
+        <input type="text" placeholder="title" /> <br />
+        <button>Add Todo</button>
+    </div>
+}
---
+++ Frontend/src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export function Todos(){
+    return <div>
+        <h1>Code</h1>
+        <h2>Description</h2>
+        <button>Mark as completed</button>
+    </div>
+}
